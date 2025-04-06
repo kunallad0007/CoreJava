f36ef8d5ceaf933f85a15af7a13bd6dfed2ad280
kunallad0007 <kunallad0007@gmail.com>
2025-04-07 04:28:04 +0530

    Initial commit of Core Java Files

---
+++ Introduction/Introduction.java
@@ -0,0 +1,8 @@
+class Introduction 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");// Prints the statement
+		/*Completed your first code*/
+	}
+}
---
+++ Operators and Control Statements/Operators/RelationalOperators.java
@@ -0,0 +1,34 @@
+package Operators;
+
+public class RelationalOperators
+{
+	public static boolean equalsTo(int a, int b)
+	{
+		return a == b;
+	}
+	
+	public static boolean notEqualsTo(int a, int b)
+	{
+		return a != b;
+	}
+	
+	public static boolean lessThan(int a, int b)
+	{
+		return a < b;
+	}
+	
+	public static boolean lessThenEqualsTo(int a, int b)
+	{
+		return a <= b;
+	}
+	
+	public static boolean greaterThen(int a, int b)
+	{
+		return a > b;
+	}
+	
+	public static boolean greaterThenEqualsTo(int a, int b)
+	{
+		return a >= b;
+	}
+}
