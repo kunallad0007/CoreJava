4faa7e4f2e11418413e03469c334cb44b2d39681
kunallad0007 <kunallad0007@gmail.com>
2025-04-08 15:52:40 +0530

    Deleted same files to keep the repo moduler and smooth

--- CoreJava with IntelliJ/CoreJava/src/Main.java
+++ CoreJava with IntelliJ/CoreJava/src/Main.java
@@ -3,23 +3,23 @@ import JavaClasses.GetInput;
 import JavaClasses.NewArrayClass;
 public class Main {
     public static void main(String[] args) {
-        SwitchStatement newSwitch = new SwitchStatement();
-        GetInput newInput = new GetInput();
-        NewArrayClass newArray = new NewArrayClass();
 
-        //Define Array Code
+        GetInput newInput = new GetInput();
+
+
+        //Using NewArrayClass and defineArray() from JavaClasses Package
+        NewArrayClass newArray = new NewArrayClass();
         System.out.print("Enter the size of array: ");
         int n = newInput.getInt();
-//        System.out.println();
         System.out.print("Enter numbers to store in an array: ");
         int[] arr = newArray.defineArray(n);
-//        System.out.println();
         newArray.printArray(arr);
 
-        //Check Vowel Code
         /*
+        //SwitchStatement newSwitch = new SwitchStatement();
+        //Check Vowel Code
         char ch = newInput.getChar("Enter any character: ");
         newSwitch.checkVowels(ch);
-         */
+        */
     }
 }
--- Introduction/Introduction.java
+++
@@ -1,8 +0,0 @@
-class Introduction 
-{
-	public static void main(String[] args) 
-	{
-		System.out.println("Hello World!");// Prints the statement
-		/*Completed your first code*/
-	}
-}
