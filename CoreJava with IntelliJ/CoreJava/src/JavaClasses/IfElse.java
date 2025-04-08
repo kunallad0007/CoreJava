2aaa79aa1f593ffcb12364ee2b6008edc5d403a6
kunallad0007 <kunallad0007@gmail.com>
2025-04-08 16:20:04 +0530

    Created new Class IfElse and using it in the Main.java File (Having fun playing with the repository - practice purpose)

---
+++ CoreJava with IntelliJ/CoreJava/src/JavaClasses/IfElse.java
@@ -0,0 +1,12 @@
+package JavaClasses;
+
+public class IfElse {
+
+    public void checkDrivingAge(int n){
+        if(n >= 18){
+            System.out.println("You are eligible!");
+        }else{
+            System.out.println("You are not eligible!");
+        }
+    }
+}
--- CoreJava with IntelliJ/CoreJava/src/Main.java
+++ CoreJava with IntelliJ/CoreJava/src/Main.java
@@ -1,25 +1,33 @@
 import JavaClasses.SwitchStatement;
 import JavaClasses.GetInput;
 import JavaClasses.NewArrayClass;
+import JavaClasses.IfElse;
 public class Main {
     public static void main(String[] args) {
 
-        GetInput newInput = new GetInput();
+        GetInput newInput = new GetInput();//creating object
 
+        //Using IfElse and checkDrivingAge() from JavaClasses Package
+        IfElse newIfElse = new IfElse();
+        int n = newInput.getInt();
+        newIfElse.checkDrivingAge(n);
 
+        /* To use the below code just uncomment any section
         //Using NewArrayClass and defineArray() from JavaClasses Package
-        NewArrayClass newArray = new NewArrayClass();
+        NewArrayClass newArray = new NewArrayClass(); //creating object
         System.out.print("Enter the size of array: ");
         int n = newInput.getInt();
         System.out.print("Enter numbers to store in an array: ");
         int[] arr = newArray.defineArray(n);
         newArray.printArray(arr);
-
+        */
         /*
         //SwitchStatement newSwitch = new SwitchStatement();
         //Check Vowel Code
         char ch = newInput.getChar("Enter any character: ");
         newSwitch.checkVowels(ch);
         */
+
+        newInput.closeInput();
     }
 }
